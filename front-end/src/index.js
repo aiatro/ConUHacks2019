17cee299803fdf396ee86ffa6d94218eb2916d3c
Erdem <erdemkepenek1@gmail.com>
2019-01-26 19:16:29 -0500

    Created React Environment

---
+++ front-end/src/index.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import Reducers from './reducers.jsx'
+import {createStore, combineReducers} from 'redux';
+import {Provider} from 'react-redux'
+import {BrowserRouter} from "react-router-dom";
+import './index.css';
+import * as serviceWorker from './serviceWorker';
+
+const reducers = combineReducers({
+    Reducers,
+});
+const store = createStore(reducers);
+
+/*store.dispatch({type: 'addUserProfile', data: JSON.parse(localStorage.getItem('jwtToken'))})*/
+
+ReactDOM.render(
+<Provider store={store}>
+    <BrowserRouter>
+    <App />
+    </BrowserRouter>
+    </Provider>
+    , document.getElementById('root'));
+
+serviceWorker.unregister();
